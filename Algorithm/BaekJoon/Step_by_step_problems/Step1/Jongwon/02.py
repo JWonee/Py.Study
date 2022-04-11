2aeb707af69c13329391c293f005e647b5fbce92
Won's Life <75774713+akrap147@users.noreply.github.com>
2022-04-11 17:46:48 +0900

    Create 02.py

---
+++ Algorithm/BaekJoon/Step_by_step_problems/Step1/Jongwon/02.py
@@ -0,0 +1,11 @@
+// 강한친구 대한육군을 두번 출력하기. 단 칸을 띄어서
+
+
+print("강한친구 대한육군\n강한친구 대한육군")
+
+
+// \n을 이용해서 줄바꿈을 하였다.
+/n 
+To insert characters that are illegal in a string, use an escape character. 
+An escape character is a backslash \ followed by the character you want to insert.
+영어 말그대로 탈출된 캐릭터로 string안에 있어도 그대로 읽히지 않고 그 자기의 동작을 수행한다. n/
