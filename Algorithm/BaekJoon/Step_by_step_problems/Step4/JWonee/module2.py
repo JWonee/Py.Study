09f444d8df758dc78b55118e54866c2f6bf254ce
JWonee <86338660+JWonee@users.noreply.github.com>
2022-05-08 22:38:58 +0900

    Delete Algorithm/BaekJoon/Step_by_step_problems/Step4/JWonee directory

--- Algorithm/BaekJoon/Step_by_step_problems/Step4/JWonee/module2.py
+++
@@ -1,8 +0,0 @@
-numbers = []
-
-for i in range(9):
-    i = int(input('numbers = '))
-    numbers.append(i)
-
-print(max(numbers))
-print(numbers.index(max(numbers))+1) #index 함수로 리스트의 max의 위치를 찾음 >> 0부터 시작하므로 +1
--- Algorithm/BaekJoon/Step_by_step_problems/Step4/JWonee/module3.py
+++
@@ -1,7 +0,0 @@
-A,B,C = map(int,input('A,B,C=').split())
-
-product = list(str(A*B*C)) 
-
-for i in range(10):
-    print(product.count(str(i)))
-
